b50a5db2e98a57f70515d4040291b740af88e595
CGJI58 <leemsk22@naver.com>
2023-07-13 14:08:54 +0900

    fetching api & hide api key

--- next.config.js
+++ next.config.js
@@ -1,6 +1,27 @@
 /** @type {import('next').NextConfig} */
+
+const BASE_URL = "https://api.themoviedb.org/3/movie/popular";
+const API_KEY = process.env.API_KEY;
+
 const nextConfig = {
   reactStrictMode: true,
-}
+  async redirects() {
+    return [
+      {
+        source: "/old/:path*",
+        destination: "/new/:path*",
+        permanent: false,
+      },
+    ];
+  },
+  async rewrites() {
+    return [
+      {
+        source: "/api/movies",
+        destination: `${BASE_URL}?api_key=${API_KEY}`,
+      },
+    ];
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
